efaae3b68cd68025b4de57fe6cc854a70954b2e1
jolerus <henrik@jolerus.io>
2021-04-14 04:35:07 +0200

    Add progress on single scrape function

--- scraping/scraper.go
+++ scraping/scraper.go
@@ -32,12 +32,14 @@ func fullScrape() {
 
 	resp, err := http.Get(aptsListLink)
 	if err != nil {
+		println("Could not get: " + aptsListLink)
 		return
 	}
 	defer resp.Body.Close()
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
+		println("Could pase HTML from: " + aptsListLink)
 		return
 	}
 
@@ -47,7 +49,7 @@ func fullScrape() {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					re := regexp.MustCompile(`(?:refid=)(.*)(?:\\)`)
+					re := regexp.MustCompile(`refid(.*)\\`)
 					link := a.Val
 					refID := re.FindString(link)
 					aptRefIDs = append(aptRefIDs, refID[6:len(refID)-1])
@@ -62,6 +64,38 @@ func fullScrape() {
 	f(doc)
 }
 
+func singleScrape(refID string) {
+	aptLink := "https://sssb.se/widgets/?widgets%5B%5D=objektinformation%40lagenheter&widgets%5B%5D=objektdokument&widgets%5B%5D=objektintressestatus&widgets%5B%5D=objektintresse&callback=a&refid=" + refID
+
+	resp, err := http.Get(aptLink)
+	if err != nil {
+		println("Could not get " + aptLink)
+		return
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		println("Could pase HTML from: " + aptLink)
+		return
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && (n.Data == "dt" || n.Data == "dd") {
+			for _, a := range n.Attr {
+				if a.Key == "class" {
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+}
+
 func main() {
-	fullScrape()
+	singleScrape("366770596d4e456d53794b38477a64794857612f5777426d4458633045414750")
 }
