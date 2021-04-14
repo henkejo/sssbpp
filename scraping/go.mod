008a508e8ddc64951bdcaacd296be496da4b4032
jolerus <henrik@jolerus.io>
2021-04-14 02:38:19 +0200

    Add gettting 'a' elements from sssb api

---
+++ scraping/go.mod
@@ -0,0 +1,5 @@
+module sssbpp-scraper
+
+go 1.16
+
+require golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
--- scraping/scraper.go
+++ scraping/scraper.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	resp, err := http.Get("https://sssb.se/widgets/?callback=jQuery17206581158882629974_1615556659893&widgets%5B%5D=objektlista%40lagenheter")
+	aptsListLink := "https://sssb.se/widgets/?callback=a&widgets%5B%5D=objektlista%40lagenheter"
+
+	resp, err := http.Get(aptsListLink)
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	print(string(body))
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return
+	}
+	// Recursively visit nodes in the parse tree
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
 }
