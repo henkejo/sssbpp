0feef79101ee881ec1fd8ab42daaf5412fa8860b
jolerus <henrik@jolerus.io>
2021-04-19 01:49:11 +0200

    Fix 3hr scheduler

--- scraping/scraper.go
+++ scraping/scraper.go
@@ -249,9 +249,7 @@ func main() {
 	scheduler := gocron.NewScheduler(t)
 
 	log.Println("First full scrape at approx: " + string(time.Now().Add(time.Hour*3).Format("01-02-2006 15:04:05")))
-	scheduler.Every(3).Hours().Do(func() {
-		fullScrape(dbmap)
-	})
+	scheduler.Every(3).Hours().Do(fullScrape, dbmap)
 
 	scheduleNextFinalScrape(dbmap, scheduler)
 
