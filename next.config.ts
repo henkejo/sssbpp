90049beab2bce62dcc9d5353396fa0afee0d4f26
henkejo <henrik.jolerus@gmail.com>
2025-12-14 02:39:34 +0000

    Fix webpack / turbopack conflict

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@sparticuz/chromium');
-    }
-    return config;
-  },
 }
 
 export default nextConfig
